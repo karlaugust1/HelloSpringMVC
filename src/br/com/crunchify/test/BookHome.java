1f4793730640a52ef0e73ddd5ce570c87c68c95b
Karl August Harder <karl.harder@roit.com.br>
2018-06-18 09:48:39 -0300

    Inclusão do Hibernate

---
+++ src/br/com/crunchify/test/BookHome.java
@@ -0,0 +1,68 @@
+package br.com.crunchify.test;
+// Generated 18/06/2018 09:45:04 by Hibernate Tools 4.0.1.Final
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Home object for domain model class Book.
+ * @see br.com.crunchify.test.Book
+ * @author Hibernate Tools
+ */
+@Stateless
+public class BookHome {
+
+	private static final Log log = LogFactory.getLog(BookHome.class);
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	public void persist(Book transientInstance) {
+		log.debug("persisting Book instance");
+		try {
+			entityManager.persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void remove(Book persistentInstance) {
+		log.debug("removing Book instance");
+		try {
+			entityManager.remove(persistentInstance);
+			log.debug("remove successful");
+		} catch (RuntimeException re) {
+			log.error("remove failed", re);
+			throw re;
+		}
+	}
+
+	public Book merge(Book detachedInstance) {
+		log.debug("merging Book instance");
+		try {
+			Book result = entityManager.merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Book findById(Integer id) {
+		log.debug("getting Book instance with id: " + id);
+		try {
+			Book instance = entityManager.find(Book.class, id);
+			log.debug("get successful");
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+}
